2d1fee05c189587da6a492d1ec43c8f7e25e4cd4
Xavih830 <120606644+Xavih830@users.noreply.github.com>
2025-10-22 20:00:02 +0000

    Fin Guia 08

--- js/file01.js
+++ js/file01.js
@@ -7,6 +7,17 @@ const showToast = () => {
     }
 };
 
+const showVideo = () => {
+    const demo = document.getElementById("demo");
+    if (demo) {
+        demo.addEventListener("click", () => {
+            window.open("https://www.youtube.com/watch?v=dQw4w9WgXcQ", "_blank");
+            /*se tiene poder sobre toda la ventana*/
+        });
+    }
+};
+
 (() => {
     showToast();
+    showVideo();
 })();
