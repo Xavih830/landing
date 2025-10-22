6aa7d4c30adfdc5a20438f4ab31095944d702775
Xavih830 <120606644+Xavih830@users.noreply.github.com>
2025-10-22 19:53:31 +0000

    Pre-DOM Guia 08

--- js/file01.js
+++ js/file01.js
@@ -1,1 +1,13 @@
 "use strict";
+
+const showToast = () => {
+    const toast = document.getElementById("toast-interactive");
+    if (toast) {
+        toast.classList.add("md:block");
+    }
+};
+
+(() => {
+    alert("Bienvenido/a");
+    console.log("Bienvenido/a");
+})();
